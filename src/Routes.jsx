2c42f9acb1df736b6e6838dec736ab0a99365a58
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-18 10:01:23 -0300

    created routes file

---
+++ src/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter, Routes as Switch, Route} from 'react-router-dom'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+    {/* adicionar header, switch e route de cada page */}
+    </BrowserRouter>
+  )
+}
+
+export default Routes

c21482e86be87e495def5e42028549acd962b52a
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-21 11:43:20 -0300

    added paths to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import S from './Header.module.css'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+
+    const navigate = useNavigate()
+
+    function handleClientClick() {
+        navigate('/clientes')
+    }
+
+    function handleFormClick() {
+        navigate('/')
+    }
+
   return (
     <header className={S.container}>
       <h1>FillForm</h1>
       <nav>
         {/* mudar para Link, precisa ser nav bar lateral*/}
-        <a className={S.link}>Clientes</a>
-        <a className={S.link}>Cadastro</a>
+        <a className={S.link} onClick={handleClientClick}>Clientes</a>
+        <a className={S.link} onClick={handleFormClick}>Cadastro</a>
       </nav>
     </header>
   )

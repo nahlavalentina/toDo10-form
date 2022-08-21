c1d99b0c710e0dbc4f3dcbffe7b9778e69037c66
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-21 10:13:46 -0300

    form one base

--- src/pages/FormOne/FormOne.jsx
+++ src/pages/FormOne/FormOne.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Button from '../../components/Button/Button'
+import Fieldset from '../../components/Fieldset/Fieldset'
 
 const FormOne = () => {
   return (
     <div>
-      
+      <Fieldset title='Nome:'
+      type='text'/>
+      <Fieldset title='Sobrenome:'
+      type='text'/>
+      <Fieldset title='Email:'
+      type='text'/>
+      <Fieldset title='Telefone:'
+      type='number'/>
+      <Button text='Próximo'/>
     </div>
   )
 }

b8bd5fcf021381ea09628d574791c60cb8b60fb1
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-21 07:35:33 -0300

    load clients function

--- src/pages/Clients/Clients.jsx
+++ src/pages/Clients/Clients.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const Clients = () => {
+    const instance = axios.create({
+        baseURL: 'https://randomuser.me/api/'
+    })
+
+    const getClients = async () => {
+        const response = await instance.get();
+        const json = response.data;
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getClients()
+    })
+
   return (
     <div>
       

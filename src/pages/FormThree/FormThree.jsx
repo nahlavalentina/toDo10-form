a082c995e04a4c0b6a4993c2e98db5a41c412b82
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-21 11:35:33 -0300

    form two and three

--- src/pages/FormThree/FormThree.jsx
+++ src/pages/FormThree/FormThree.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Fieldset from '../../components/Fieldset/Fieldset'
+import Button from '../../components/Button/Button'
 
 const FormThree = () => {
   return (
     <div>
-      
+      <Fieldset title='Data de Nascimento:'
+      type='date'/>
+      <Fieldset title='CPF:'
+      type='number'/>
+      <Fieldset title='Renda Mensal:'
+      type='number'/>
+      <Button text='Salvar'/>
     </div>
   )
 }
--- src/pages/FormTwo/FormTwo.jsx
+++ src/pages/FormTwo/FormTwo.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Fieldset from "../../components/Fieldset/Fieldset";
+import Button from "../../components/Button/Button";
 
 const FormTwo = () => {
   return (
     <div>
-      
+      <Fieldset title="CEP:" type="number" />
+      <Fieldset title="Endereço:" type="text" />
+      <Fieldset title="Complemento:" type="text" />
+      <Button text="Próximo" />
     </div>
-  )
-}
+  );
+};
 
-export default FormTwo
+export default FormTwo;

e5b97ea02a1381b003247fe1302903c6f130726d
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-23 11:10:40 -0300

    validate on page two

--- src/pages/FormTwo/FormTwo.jsx
+++ src/pages/FormTwo/FormTwo.jsx
@@ -1,16 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Fieldset from "../../components/Fieldset/Fieldset";
 import Button from "../../components/Button/Button";
 
 const FormTwo = () => {
-  return (
-    <div>
-      <Fieldset title="CEP:" type="number" />
-      <Fieldset title="Endereço:" type="text" />
-      <Fieldset title="Complemento:" type="text" />
-      <Button text="Próximo" />
-    </div>
-  );
+    const [values, setValues] = useState({
+        cep: "",
+        address: "",
+        complement: ""
+      });
+      const navigate = useNavigate()
+    
+      const [errors, setErrors] = useState({
+        type: "",
+        message: ""
+      });
+    
+      const handleChange = (target, key) => {
+        const value = target.value;
+        setValues({
+          ...values,
+          [key]: value
+        });
+        console.log(values);
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+       if (!validate()) return
+    
+       setValues({
+        cep: values.cep,
+        address: values.address,
+        complement: values.complement
+       })
+    
+       console.log(values)
+       console.log(errors)
+    
+       alert("Uhum")
+    
+        navigate("/dados-finalizar")
+      }
+    
+    
+    function validate() {
+        if (!values.cep.trim()) {
+            return setErrors({
+                type: "error",
+                message: "Necessário preencher CEP!"
+            })
+        } else if (values.cep.length != 8) {
+            return setErrors({
+                type: "error",
+                message: "CEP inválido!"
+            })
+        }
+    
+        if (!values.address.trim()) {
+            return setErrors({
+                type: "error",
+                message: "Necessário preencher endereço!"
+            })
+        }
+    
+        if (!values.complement) {
+            return setErrors({
+                type: "error",
+                message: "Necessário preencher complemento!"
+            })
+        }
+
+        return true;
+      }
+    
+      return (
+        <div>
+          <div>
+            <Fieldset
+              title="CEP:"
+              type="number"
+              name="cep"
+              placeholder="Escreva seu CEP"
+              value={values.cep}
+              onchange={({ target }) => handleChange(target, "cep")}
+            />
+             {errors.type === "error" && <p>{errors.message}</p>}
+          </div>
+          <div>
+            <Fieldset
+              title="Endereço:"
+              type="text"
+              name="address"
+              placeholder="Escreva seu endereço"
+              value={values.address}
+              onchange={({ target }) => handleChange(target, "address")}
+            />
+             {errors.type === "error" && <p>{errors.message}</p>}
+          </div>
+          <div>
+            <Fieldset
+              title="Complemento:"
+              type="text"
+              name="complement"
+              placeholder="Escreva o complemento"
+              value={values.complement}
+              onchange={({ target }) => handleChange(target, "complement")}
+            />
+             {errors.type === "error" && <p>{errors.message}</p>}
+          </div>
+          <Button text="Próximo" onclick={handleSubmit} />
+        </div>
+      );
 };
 
 export default FormTwo;
